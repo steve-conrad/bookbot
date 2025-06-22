b245e8659931739bc49aaa92577302e7a3805743
steve-conrad <steve.conrad14@gmail.com>
2025-06-21 22:01:47 -0400

    added word count

--- main.py
+++ main.py
@@ -6,7 +6,18 @@ def get_book_text(r):
         with open(r) as f:
             read_data = f.read()
         return read_data
+
+def get_word_count():
+    contents = get_book_text(book_path)
+    count = 0
+    split = contents.split()
+    for word in split:
+        count += 1
+    return count
+
 def main():
     output = get_book_text(book_path)
-    print(output)
+    num_words = get_word_count()
+    print(num_words, "words found in the document")
+
 main()
